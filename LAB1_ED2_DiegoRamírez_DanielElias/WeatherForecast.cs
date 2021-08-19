d5ba9bbb7ad5b47cadc4e83140d144ea54b23d14
Dandres2001 <dandres21nw@gmail.com>
2021-08-19 14:47:28 -0600

    Agregar archivos de proyecto.

---
+++ LAB1_ED2_DiegoRamírez_DanielElias/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LAB1_ED2_DiegoRamírez_DanielElias
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
