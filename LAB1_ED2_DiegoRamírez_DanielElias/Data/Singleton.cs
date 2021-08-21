a2a7959bf656df88468cdd001b44470cde250bf8
Dandres2001 <dandres21nw@gmail.com>
2021-08-20 18:04:40 -0600

    Configuración de API, Singleton y modelo.

---
+++ LAB1_ED2_DiegoRamírez_DanielElias/Data/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB1_ED2_DiegoRamírez_DanielElias.Data
+{
+    public sealed class Singleton
+    {
+        private readonly static Singleton _instance = new Singleton();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
---
+++ LAB1_ED2_DiegoRamírez_DanielElias/Models/Values.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB1_ED2_DiegoRamírez_DanielElias.Models
+{
+    public class Values
+    {
+    }
+}
